4d930dac81ba67294254c7c30406469237b05ab7
ArildoJunior <arildoajunior@gmail.com>
2021-05-09 14:48:26 -0300

    Alteração Propeties / Resource

--- src/main/java/com/github/arildo/junior/citiesapi/CountryResource.java
+++ src/main/java/com/github/arildo/junior/citiesapi/CountryResource.java
@@ -3,21 +3,21 @@ package com.github.arildo.junior.citiesapi;
 import com.github.arildo.junior.citiesapi.countries.Country;
 import com.github.arildo.junior.citiesapi.repository.CoutryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
-import java.util.Scanner;
+import java.awt.print.Pageable;
 
 @RestController
 @RequestMapping("/countries")
 public class CountryResource {
 
     @Autowired
-    private CoutryRepository repositpry;
+    private CoutryRepository repository;
 
     @GetMapping
-    public List<Country> countries() {
-        return repositpry.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll((org.springframework.data.domain.Pageable) page);
     }
 }
